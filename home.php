ed970097759d098233ea73eedfddfb7dd4dbfbe6
ardanikov <sess.uuai@gmail.com>
2023-01-01 11:09:13 +0700

    add search bar

--- home.php
+++ home.php
@@ -94,13 +94,15 @@ if (isset($_POST["updateGuru"])) {
         <div class="row">
           <div class="col">
             <h2 style="margin: 0; width:fit-content; height:fit-content;">Daftar Siswa</h2>
+            <input type="text" onkeyup="searchNameSiswa()" placeholder="Cari Nama" id="SearchNamaSiswa">
           </div>
           <div class="col">
+            <input type="text" class="none">
             <button class="button-tambah" id="btnAdd"><i class="bi bi-plus-lg"></i> Tambah Data</button>
           </div>
         </div>
         <div class="row">
-          <table class="styled-table display" id="table_id">
+          <table class="styled-table display table-sortable" id="table_siswa">
             <thead>
               <tr>
                 <th>No</th>
@@ -160,13 +162,15 @@ if (isset($_POST["updateGuru"])) {
         <div class="row">
           <div class="col">
             <h2 style="margin: 0; width:fit-content; height:fit-content;">Daftar Guru</h2>
+            <input type="text" onkeyup="searchNameGuru()" placeholder="Cari Nama" id="SearchNamaGuru">
           </div>
           <div class="col">
+            <input type="text" class="none">
             <button class="button-tambah" id="btnAddGuru"><i class="bi bi-plus-lg"></i> Tambah Data</button>
           </div>
         </div>
         <div class="row">
-          <table class="styled-table display" id="table_id">
+          <table class="styled-table display table-sortable" id="table_guru">
             <thead>
               <tr>
                 <th>No</th>
@@ -485,6 +489,48 @@ if (isset($_POST["updateGuru"])) {
     var spanGuruUpdate = document.getElementsByClassName("closeGuruUpdate")[0];
     var spanGuru = document.getElementsByClassName("closeGuru")[0];
     var dataModal = {};
+
+    function searchNameGuru() {
+      console.log('working');
+      let input, filter, table, tr, td, txtValue;
+      input = document.getElementById("SearchNamaGuru")
+      filter = input.value.toUpperCase()
+      table = document.getElementById("table_guru")
+      tr = table.getElementsByTagName("tr");
+      for (let i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[2]
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = ""
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
+
+      }
+    }
+
+    function searchNameSiswa() {
+      console.log('working');
+      let input, filter, table, tr, td, txtValue;
+      input = document.getElementById("SearchNamaSiswa")
+      filter = input.value.toUpperCase()
+      table = document.getElementById("table_siswa")
+      tr = table.getElementsByTagName("tr");
+      for (let i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[2]
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = ""
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
+
+      }
+    }
     span.onclick = function() {
       modal.style.display = "none";
     }
@@ -731,6 +777,43 @@ if (isset($_POST["updateGuru"])) {
       // document.getElementById("tahunawalguruupdate").value = params['tahunmengajar'].slice(-9, -5);
       // document.getElementById("tahunakhirguruupdate").value = params['tahunmengajar'].slice(-4);
     }
+
+    function sortTableByColumn(table, column, asc = true) {
+      const dirModifier = asc ? 1 : -1;
+      const tBody = table.tBodies[0];
+      const rows = Array.from(tBody.querySelectorAll("tr"));
+
+      // Sort each row
+      const sortedRows = rows.sort((a, b) => {
+        const aColText = a.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+        const bColText = b.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+
+        return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+      });
+
+      // Remove all existing TRs from the table
+      while (tBody.firstChild) {
+        tBody.removeChild(tBody.firstChild);
+      }
+
+      // Re-add the newly sorted rows
+      tBody.append(...sortedRows);
+
+      // Remember how the column is currently sorted
+      table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+      table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-asc", asc);
+      table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-desc", !asc);
+    }
+
+    document.querySelectorAll(".table-sortable th").forEach(headerCell => {
+      headerCell.addEventListener("click", () => {
+        const tableElement = headerCell.parentElement.parentElement.parentElement;
+        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+        const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+
+        sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
+      });
+    });
   </script>
 
 </body>
